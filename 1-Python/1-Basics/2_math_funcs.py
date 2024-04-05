149fe254198fb5eb87f7cb2a8f78f654d54546ce
Amlan Alok <amlanalok@gmail.com>
2024-04-05 16:00:31 -0500

    added python math func

--- 1-Python/1-Basics/2_math_funcs.py
+++ 1-Python/1-Basics/2_math_funcs.py
@@ -13,8 +13,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(4 - 2, 2)
         self.assertEqual(4 * 2, 8)
         self.assertEqual(4 / 2, 2)
-        self.assertEqual(5 // 2, 2)     # floor operation
-        self.assertEqual(-5 // 2, 2)     # floor operation
+
+        self.assertEqual(5 // 2, 2)  # floor operation
+        self.assertEqual(-5 // 2, -3)  # floor operation
+        self.assertEqual(5 % 2, 1)  # modulo operation
+        self.assertEqual(4 % 2, 0)  # modulo operation
+
+        self.assertEqual(max(6, 2), 6)
+        self.assertEqual(min(6, 2), 2)
+        self.assertEqual(max([1, 2, 3]), 3)
+        self.assertEqual(min([1, 2, 3]), 1)
 
     def test_strings_comparison(self):
         """
