a1736d1e7b58b194ca0d2a93f00cf26e54dde864
Amlan Alok <amlanalok@gmail.com>
2024-06-17 17:12:14 -0500

    Create comments.py

---
+++ 4-Website/2-Comments/comments.py
@@ -0,0 +1,8 @@
+# single line comment
+
+"""
+Python does not support multi line comments.
+But it does allow to write doc strings like used here.
+They are not comments but achieve the same idea.
+Their real purpose is to be used to write documentation text.
+"""
