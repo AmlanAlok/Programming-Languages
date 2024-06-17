cc7c4766e7e19e3a1085bdd9b882fc2faae33e3c
Amlan Alok <amlanalok@gmail.com>
2024-06-17 17:17:19 -0500

    Update Comments.java

--- 4-Website/2-Comments/Comments.java
+++ 4-Website/2-Comments/Comments.java
@@ -1,13 +1,13 @@
 
 public class Comments {
     public static void main(Strings[] args){
-      // signle line comment
+    	// This is a single-line comment
+        int x = 5; // This is an inline comment
 
-      /*
-      This is a multi-line comment.
-      This is how it is done
-      */
-      System.out.println('Comments');
-      
+    	/*
+      	This is a multi-line comment.
+      	This is how it is done
+      	*/
+      	System.out.println('Comments');
     }
 }
