0d4d074e34dd34c318d2179971ada97313f6e8a3
Amlan Alok <amlanalok@gmail.com>
2024-06-17 17:14:37 -0500

    Create Comments.java

---
+++ 4-Website/2-Comments/Comments.java
@@ -0,0 +1,13 @@
+
+public class Comments {
+    public static void main(Strings[] args){
+      // signle line comment
+
+      /*
+      This is a multi-line comment.
+      This is how it is done
+      */
+      System.out.println('Comments');
+      
+    }
+}
