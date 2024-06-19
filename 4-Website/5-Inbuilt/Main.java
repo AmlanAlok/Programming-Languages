8122efd74e606dbaca61b8a85f7b09ae6b225e8a
Amlan Alok <amlanalok@gmail.com>
2024-06-19 17:56:30 -0500

    Create Main.java

---
+++ 4-Website/5-Inbuilt/Main.java
@@ -0,0 +1,24 @@
+
+int x = Math.max(5, 10);
+System.oput.println(x);
+
+int y = Math.min(5, 10);
+System.oput.println(y);
+
+int time = 22;
+if (time < 10) {
+  System.out.println("Good morning.");
+} else if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+// Outputs "Good evening."
+
+// variable = (condition) ? expressionTrue :  expressionFalse;
+
+// Arrays
+
+int[] myNum = {10, 20, 30, 40};
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+System.out.println(cars.length);      // Outputs 4
