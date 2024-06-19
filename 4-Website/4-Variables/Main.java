45fa6537febad335e1689691ba29363018e63b21
Amlan Alok <amlanalok@gmail.com>
2024-06-19 15:35:43 -0500

    Create Main.java

---
+++ 4-Website/4-Variables/Main.java
@@ -0,0 +1,46 @@
+
+// type variableName = value;
+
+String name = "John";
+System.out.println(name);
+
+int myNum = 15;
+System.out.println(myNum);
+
+int myNum;
+myNum = 15;
+System.out.println(myNum);
+
+final int myNum = 15;
+myNum = 20;  // will generate an error: cannot assign a value to a final variable
+
+int myNum = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+
+// You can also assign the same value to multiple variables in one line
+int x, y, z;
+x = y = z = 50;
+System.out.println(x + y + z);
+
+/*
+https://www.w3schools.com/java/java_data_types.asp
+
+Primitive data types - includes:
+1. byte           1 byte	    Stores whole numbers from -128 to 127
+2. short          2 bytes	    Stores whole numbers from -32,768 to 32,767
+3. int            4 bytes	    Stores whole numbers from -2,147,483,648 to 2,147,483,647
+4. long           8 bytes	    Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+5. float          4 bytes	    Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+6. double         8 bytes	    Stores fractional numbers. Sufficient for storing 15 decimal digits
+7. boolean        1 bit	      Stores true or false values
+8. char           2 bytes	    Stores a single character/letter or ASCII values
+
+Non-primitive data types
+1. String
+2. Arrays
+3. Classes
+  */
+
